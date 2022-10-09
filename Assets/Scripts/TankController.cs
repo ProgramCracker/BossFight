f2ad6656d4a5b682ddc67b927ae66f636804e4f0
Trevor <the.trevor.miller@gmail.com>
2022-10-09 17:18:40 -0500

    Turret Experimentation
    Tried to get the turret to be effected by a turnspeed but it doesn't seem to work with the system I'm currently using. I don't want to redo what I have already so I'm keeping it the way it is.

--- Assets/Scripts/TankController.cs
+++ Assets/Scripts/TankController.cs
@@ -1,31 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class TankController : MonoBehaviour
 {
+    [SerializeField] float _turnSpeed = 2f;
+    [SerializeField] Camera mainCamera;
+    //[SerializeField] private LayerMask groundMask;
+    //[SerializeField] public float _turretRotSpeed = 5f;
     [SerializeField] float _maxSpeed = .25f;
+
+    Rigidbody _rb = null;
+    public Transform _turretBase;
+
+    //public UnityEvent<Vector3> OnMoveTurret = new UnityEvent<Vector3>();
+
     public float MaxSpeed
     {
         get => _maxSpeed;
         set => _maxSpeed = value;
     }
-    [SerializeField] float _turnSpeed = 2f;
-
-
-
-    Rigidbody _rb = null;
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody>();
+        mainCamera = Camera.main;
     }
 
     private void FixedUpdate()
     {
         MoveTank();
         TurnTank();
+        //GetTurretMovement();
+
     }
+
 
     public void MoveTank()
     {
@@ -47,4 +57,47 @@ public class TankController : MonoBehaviour
         // apply quaternion to the rigidbody
         _rb.MoveRotation(_rb.rotation * turnOffset);
     }
+
+    /*
+   
+    // GETTING mouse position on screen to be position in world
+    private void GetTurretMovement()
+    {
+        OnMoveTurret?.Invoke(GetMousePos());
+    }
+    private Vector3 GetMousePos()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.y = mainCamera.nearClipPlane;
+        Vector3 mouseWorldPosition = mainCamera.ScreenToWorldPoint(mousePos);
+        return mouseWorldPosition;
+    }
+
+
+    public void TurnTurret(Vector3 mousePos)
+    {
+        var turretdirection = (Vector3)mousePos - _turretBase.position;
+
+        var desiredAngle = Mathf.Atan2(turretdirection.y, turretdirection.x) * Mathf.Rad2Deg;
+
+        var rotationPeriod = _turretRotSpeed * Time.deltaTime;
+
+        _turretBase.rotation = Quaternion.RotateTowards(_turretBase.rotation, Quaternion.Euler(0,desiredAngle -90,0), rotationPeriod);
+    }
+
+    /*
+    private (bool success, Vector3 position) GetMousePosition()
+    {
+        var ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out var hitInfo, Mathf.Infinity, groundMask))
+        {
+            return (success: true, position: hitInfo.point);
+        }
+        else
+        {
+            return (success: false, position: Vector3.zero);
+        }
+    }
+    */
 }
--- Assets/Scripts/TurretMove.cs
+++ Assets/Scripts/TurretMove.cs
@@ -6,8 +6,8 @@ using UnityEngine;
 public class TurretMove : MonoBehaviour
 {
 
-
     [SerializeField] private LayerMask groundMask;
+    [SerializeField] public float _turnSpeed = .5f;
 
 
     [SerializeField] Camera mainCamera;
@@ -20,20 +20,26 @@ public class TurretMove : MonoBehaviour
 
     private void Update()
     {
-        Aim();
+        Aim(_turnSpeed);
     }
 
-    private void Aim()
+    private void Aim(float Speed)
     {
         var (success, position) = GetMousePosition();
         if (success)
         {
             // direction math
-            var direction = position - transform.position;
+            var direction = (position - transform.position);
 
-            direction.y = 0; // no height
+            //direction.y = 0; // no height
+
+            float turnSpeed = Speed * Time.deltaTime;
 
             transform.forward = direction;
+            //transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, position.y, 0), turnSpeed);
+
+            //transform.localRotation = Quaternion.Euler(aimSpeed, direction.y,aimSpeed);
+
         }
     }
 
